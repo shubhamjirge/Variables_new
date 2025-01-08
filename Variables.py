c0417073683b81ec6ae1cb98065caecd9f387e88
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-01-08 18:18:49 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#VARIBLES
+name = "jack"
+print(name)
+
+name = "angela"
+print(name)
+
+# Check the length of the user input.
+print(len(input("what is your name?")))
+
+# Split everything into variables.
+username = (input("what is your name?"))
+length = len(username)
+print(length)
+
